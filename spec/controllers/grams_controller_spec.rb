1dec6eadbcb85835c0639ac741de08dcac1fd7fe
Julian Smith <smith.julian.f@gmail.com>
2019-12-12 06:16:06 +0000

    Created tests for page to show details for a gram. Course recommended commit before attempting solution

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+	describe "grams#show action" do
+		it "should successfully show the page if the gram is found" do
+			gram = FactoryBot.create(:gram)
+			get :show, params: { id: gram.id }
+			expect(response).to have_http_status(:success)
+		end
+
+		it "should return a 404 error if the gram is not found" do
+			get :show, params: { id: 'GatoDelTaco' }
+			expect(response).to have_http_status(:not_found)
+		end
+	end
+
+
 	describe "grams#index action" do
 		it "should successfully show the page" do
 			get :index
@@ -23,7 +37,7 @@ RSpec.describe GramsController, type: :controller do
 		end
 	end
 
-	describe "rams#create action" do
+	describe "grams#create action" do
 
 		it "should require users to be logged in" do
 			post :create, params: { gram: { message: "Hello" } }
